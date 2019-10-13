d92ab68800bb72a5c41d52135ad4a257c7e05357
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-10-13 11:53:29 +0100

    setup for single book add search section to all books

--- books/views.py
+++ books/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# return all Books
 def books(request):
   return render(request, 'books/books.html')
+
+# return single book
+def book(request, book_id):
+  return render(request, 'books/book.html')
