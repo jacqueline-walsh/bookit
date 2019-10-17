3768721983cce68159a8054eb0965a59c852a3a9
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-10-17 09:14:51 +0100

    add message alerts to registration and login forms

--- bookit/settings.py
+++ bookit/settings.py
@@ -130,3 +130,9 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, './media')
+
+# messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger'
+}
