635030558319c46080f274879bc3669d1df10404
jacqueline-walsh <jacqueline.walsh34@gmail.com>
2019-12-28 18:16:02 +0000

    settings file update

--- bookit/settings.py
+++ bookit/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 from django.contrib.messages import constants as messages
 import os
-import env
+# import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
